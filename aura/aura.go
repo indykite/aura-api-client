1bc76b6d33c0546ada00d01abcbcc170e4d85d57
simon <simon.thordal@indykite.com>
2024-01-09 11:36:41 +0100

    fixes and adding GetInstance

--- aura/aura.go
+++ aura/aura.go
@@ -14,7 +14,8 @@ const endpoint = "https://api.neo4j.io/v1"
 
 // Client is the interface containing the methods for connecting to the Aura API.
 type Client interface {
-	CreateInstance() (*CreateResponse, error)
+	CreateInstance(name string) (*CreateResponse, error)
+	GetInstance(name string) (*GetResponse, error)
 }
 
 type client struct {
@@ -51,7 +52,7 @@ func WithHTTPClient(h http.Client) Option {
 	}
 }
 
-// WithEndpoint sets the a custom endpoint for Aura
+// WithEndpoint sets the a custom endpoint for Aura.
 func WithEndpoint(e string) Option {
 	return func(c *client) {
 		c.endpoint = e
@@ -59,21 +60,22 @@ func WithEndpoint(e string) Option {
 }
 
 // CreateResponse is returned when creating an Aura instance and is
-// Constructed from the values from
-// https://neo4j.com/docs/aura/platform/api/specification/#/instances/post-instances
+// constructed from the values from
+// https://neo4j.com/docs/aura/platform/api/specification/#/instances/post-instances.
 type CreateResponse struct {
-	id             string // Internal ID of the instance
-	connection_url string // URL the instance is hosted at
-	username       string // Name of the initial admin user
-	password       string // Password of the initial admin user
-	name           string // The name we chose for the instance
-	tenant_id      string // Tenant for managing Aura console users
-	cloud_provider string // GCP, AWS, ...
-	region         string // us-east1, eu-central2, ...
-	instance_type  string // enterprise-db, professional-db, ...
+	ID            string // Internal ID of the instance
+	ConnectionURL string // URL the instance is hosted at
+	Username      string // Name of the initial admin user
+	Password      string // Password of the initial admin user
+	Name          string // The name we chose for the instance
+	TenantID      string // Tenant for managing Aura console users
+	CloudProvider string // GCP, AWS, ...
+	Region        string // us-east1, eu-central2, ...
+	InstanceType  string // enterprise-db, professional-db, ...
 }
 
-// Clean this up with JSON tags
+// NewCreateResponse attempts to construct a CreateResponse struct from a given
+// http response
 func NewCreateResponse(httpResp *http.Response) (*CreateResponse, error) {
 	body, err := UnmarshalResponse(httpResp)
 	if err != nil {
@@ -84,39 +86,41 @@ func NewCreateResponse(httpResp *http.Response) (*CreateResponse, error) {
 		return nil, errors.New(`expected response to be a map with string keys`)
 	}
 	resp := &CreateResponse{}
-	if resp.id, ok = m["id"].(string); !ok {
+	if resp.ID, ok = m["id"].(string); !ok {
 		return nil, errors.New(`response missing key "id" or value not string`)
 	}
-	if resp.connection_url, ok = m["connection_url"].(string); !ok {
+	if resp.ConnectionURL, ok = m["connection_url"].(string); !ok {
 		return nil, errors.New(`response missing key "connection_url" or value not string`)
 	}
-	if resp.username, ok = m["username"].(string); !ok {
+	if resp.Username, ok = m["username"].(string); !ok {
 		return nil, errors.New(`response missing key "username" or value not string`)
 	}
-	if resp.password, ok = m["password"].(string); !ok {
+	if resp.Password, ok = m["password"].(string); !ok {
 		return nil, errors.New(`response missing key "password" or value not string`)
 	}
-	if resp.name, ok = m["name"].(string); !ok {
+	if resp.Name, ok = m["name"].(string); !ok {
 		return nil, errors.New(`response missing key "name" or value not string`)
 	}
-	if resp.tenant_id, ok = m["tenant_id"].(string); !ok {
+	if resp.TenantID, ok = m["tenant_id"].(string); !ok {
 		return nil, errors.New(`response missing key "tenant_id" or value not string`)
 	}
-	if resp.cloud_provider, ok = m["cloud_provider"].(string); !ok {
+	if resp.CloudProvider, ok = m["cloud_provider"].(string); !ok {
 		return nil, errors.New(`response missing key "cloud_provider" or value not string`)
 	}
-	if resp.region, ok = m["region"].(string); !ok {
+	if resp.Region, ok = m["region"].(string); !ok {
 		return nil, errors.New(`response missing key "region" or value not string`)
 	}
-	if resp.instance_type, ok = m["instance_type"].(string); !ok {
-		return nil, errors.New(`response missing key "instance_type" or value not string`)
+	if resp.InstanceType, ok = m["type"].(string); !ok {
+		return nil, errors.New(`response missing key "type" or value not string`)
 	}
 	return resp, nil
 }
 
-// CreateInstance should take a config object
+// CreateInstance attempts to create a new Aura instance with the given name
+// returning information about the instance if succesful and otherwise
+// returning an error.
 func (c *client) CreateInstance(name string) (*CreateResponse, error) {
-	req, err := c.NewRequest("POST", endpoint+"/instances", map[string]any{
+	req, err := c.NewRequest("POST", c.endpoint+"/instances", map[string]any{
 		"name": name,
 	})
 	if err != nil {
@@ -127,6 +131,82 @@ func (c *client) CreateInstance(name string) (*CreateResponse, error) {
 		return nil, err
 	}
 	return NewCreateResponse(apiResp)
+}
+
+// GetResponse contains information about a given Aura instance and
+// is constructed from specification at
+// https://neo4j.com/docs/aura/platform/api/specification/#/instances/get-instance-id.
+type GetResponse struct {
+	ID            string // Internal ID of the instance
+	Name          string // The name we chose for the instance
+	Status        string // Indicates whether the instance is ready or under setup
+	TenantID      string // Tenant for managing Aura console users
+	CloudProvider string // GCP, AWS, ...
+	ConnectionURL string // URL the instance is hosted at
+	Region        string // us-east1, eu-central2, ...
+	InstanceType  string // enterprise-db, professional-db, ...
+	Memory        string // Amount of memory allocated, i.e. "8GB"
+	Storage       string // Amount of storage allocated, i.e. "16GB"
+}
+
+// NewGetResponse attempts to construct a GetResponse struct from a given
+// http response
+func NewGetResponse(httpResp *http.Response) (*GetResponse, error) {
+	body, err := UnmarshalResponse(httpResp)
+	if err != nil {
+		return nil, err
+	}
+	m, ok := body.(map[string]any)
+	if !ok {
+		return nil, errors.New(`expected response to be a map with string keys`)
+	}
+	resp := &GetResponse{}
+	if resp.ID, ok = m["id"].(string); !ok {
+		return nil, errors.New(`response missing key "id" or value not string`)
+	}
+	if resp.ConnectionURL, ok = m["connection_url"].(string); !ok {
+		return nil, errors.New(`response missing key "connection_url" or value not string`)
+	}
+	if resp.Name, ok = m["name"].(string); !ok {
+		return nil, errors.New(`response missing key "name" or value not string`)
+	}
+	if resp.TenantID, ok = m["tenant_id"].(string); !ok {
+		return nil, errors.New(`response missing key "tenant_id" or value not string`)
+	}
+	if resp.CloudProvider, ok = m["cloud_provider"].(string); !ok {
+		return nil, errors.New(`response missing key "cloud_provider" or value not string`)
+	}
+	if resp.Region, ok = m["region"].(string); !ok {
+		return nil, errors.New(`response missing key "region" or value not string`)
+	}
+	if resp.InstanceType, ok = m["type"].(string); !ok {
+		return nil, errors.New(`response missing key "type" or value not string`)
+	}
+	if resp.Status, ok = m["status"].(string); !ok {
+		return nil, errors.New(`response missing key "status" or value not string`)
+	}
+	if resp.Memory, ok = m["memory"].(string); !ok {
+		return nil, errors.New(`response missing key "memory" or value not string`)
+	}
+	if resp.Storage, ok = m["storage"].(string); !ok {
+		return nil, errors.New(`response missing key "storage" or value not string`)
+	}
+
+	return resp, nil
+}
+
+// GetInstnace attempts to get information about an instance identified
+// by the ID assigned to it by Neo4J.
+func (c *client) GetInstance(id string) (*GetResponse, error) {
+	req, err := c.NewRequest("GET", c.endpoint+"/instances/"+id, nil)
+	if err != nil {
+		return nil, err
+	}
+	apiResp, err := c.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	return NewGetResponse(apiResp)
 }
 
 func (c *client) DestroyInstance() {
@@ -199,7 +279,7 @@ func (c *client) authenticate() error {
 	if err != nil {
 		return err
 	}
-	req, err := http.NewRequest("POST", endpoint+"/oauth/token", bytes.NewReader(body))
+	req, err := http.NewRequest("POST", c.endpoint+"/oauth/token", bytes.NewReader(body))
 	if err != nil {
 		return err
 	}
@@ -216,15 +296,14 @@ func (c *client) authenticate() error {
 	if err != nil {
 		return err
 	}
-	// TODO switch to JSON tags
 	if _, ok := res["access_token"].(string); !ok {
 		return errors.New("auth response missing access_token key or value not string")
 	}
-	if _, ok := res["expires_in"].(int); !ok {
-		return errors.New("auth response missing expires_in key or value not integer")
+	if _, ok := res["expires_in"].(float64); !ok {
+		return errors.New("auth response missing expires_in key or value not numerical")
 	}
 	c.accessToken = res["access_token"].(string)
-	c.tokenExpiresAt = time.Now().Add(time.Second * time.Duration(res["expires_in"].(int)))
+	c.tokenExpiresAt = time.Now().Add(time.Second * time.Duration(int(res["expires_in"].(float64))))
 	return nil
 }
 
--- aura/aura_test.go
+++ aura/aura_test.go
@@ -1,28 +1,94 @@
 package aura_test
 
 import (
-	"net/http"
+	"encoding/json"
 
 	"github.com/indykite/aura-client/aura"
+	"github.com/jarcoal/httpmock"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 )
 
-const endpoint = "aura.example"
-
-func handler(w http.ResponseWriter, r *http.Request) {
-
+func mockAuth() {
+	httpmock.RegisterResponder("POST", endpoint+"/oauth/token",
+		httpmock.NewStringResponder(200, `{"access_token": "bar", "expires_in": 3600}`))
 }
 
+func mockGet(ID string) {
+	b := map[string]any{
+		"data": map[string]any{
+			"id":             ID,
+			"name":           "Production",
+			"status":         "running",
+			"tenant_id":      "YOUR_TENANT_ID",
+			"cloud_provider": "gcp",
+			"connection_url": "YOUR_CONNECTION_URL",
+			"region":         "europe-west1",
+			"type":           "enterprise-db",
+			"memory":         "8GB",
+			"storage":        "16GB",
+		},
+	}
+	body, _ := json.Marshal(b)
+	httpmock.RegisterResponder("GET", endpoint+"/instances/"+ID,
+		httpmock.NewStringResponder(200, string(body)))
+}
+
+func mockCreate(name string) {
+	b := map[string]any{
+		"data": map[string]any{
+			"id":             "db1d1234",
+			"connection_url": "YOUR_CONNECTION_URL",
+			"username":       "neo4j",
+			"password":       "letMeIn123!",
+			"tenant_id":      "YOUR_TENANT_ID",
+			"cloud_provider": "gcp",
+			"region":         "europe-west1",
+			"type":           "enterprise-db",
+			"name":           name,
+		},
+	}
+	body, _ := json.Marshal(b)
+	httpmock.RegisterResponder("POST", endpoint+"/instances",
+		httpmock.NewStringResponder(200, string(body)))
+}
+
+const endpoint = "aura.example"
+
 var _ = Describe("Aura", func() {
+	var (
+		client aura.Client
+		err    error
+	)
 	BeforeEach(func() {
-		client, err := &aura.NewClient("foo", "bar", aura.WithEndpoint(endpoint))
+		client, err = aura.NewClient("foo", "bar", aura.WithEndpoint(endpoint))
 		if err != nil {
 			panic(err)
 		}
 	})
 	Describe("Creating an instance", func() {
 		It("should create a post request to the Aura API", func() {
+			mockAuth()
+			mockCreate("foo")
+			actual, err := client.CreateInstance("foo")
+			Expect(err).To(Succeed())
+			Expect(actual.Name).To(Equal("foo"))
+		})
+	})
+	Describe("Getting an instance", func() {
+		It("should return the instance info when succesful", func() {
+			mockAuth()
+			mockGet("abc123")
+			actual, err := client.GetInstance("abc123")
+			Expect(err).To(Succeed())
+			Expect(actual.ID).To(Equal("abc123"))
+		})
+	})
+	Describe("Deleting an instance", func() {
+		It("should return the instance response when succesful", func() {
+
+		})
+		It("should treat 404 as success", func() {
 
 		})
 	})
