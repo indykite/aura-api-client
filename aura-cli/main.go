6c88d6747b34dfa9680e53a8e05308e122c3c42d
simon <simon.thordal@indykite.com>
2024-01-09 11:38:05 +0100

    rename CLI dir

--- aura-cli/main.go
+++ aura-cli/main.go
@@ -1,10 +1,9 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package main
 
-import "github.com/indykite/aura-client/live-test/cmd"
+import "github.com/indykite/aura-client/aura-cli/cmd"
 
 func main() {
 	cmd.Execute()
